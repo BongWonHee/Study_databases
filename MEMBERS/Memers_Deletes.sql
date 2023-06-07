c6012b817521ec47be2fa53398d3a5cb0b27c2d0
BongWonHee <leeban1004@gmail.com>
2023-06-07 12:17:42 +0900

    add

---
+++ MEMBERS/Memers_Deletes.sql
@@ -0,0 +1,10 @@
+-- DELETE FROM table_name WHERE condition;
+delete from members_hobby;
+delete from membewrs_login;
+delete from hobby_code;
+delete from members;
+delete from address_code;
+delete from gender_code ;
+
+
+
