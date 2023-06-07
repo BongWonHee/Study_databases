48b2b44d4b787a5495156c9806026b46a41d45a2
BongWonHee <leeban1004@gmail.com>
2023-06-07 15:46:05 +0900

    add

--- SQLS/selectwhereNotCountry.sql
+++ SQLS/selectwhereNotCountry.sql
@@ -3,3 +3,4 @@ FROM Customers
 WHERE Country NOT LIKE 'USA' AND Country NOT LIKE 'GERMANY' ;
 
 -- Number of Records: 67
+-- https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_where 출처링크
