0650e786a487b307b611902e5a2c4f2caaff79d9
BongWonHee <leeban1004@gmail.com>
2023-06-08 16:32:10 +0900

    ADD

--- SQLS/OrdersMaxEmployee.sql
+++ SQLS/OrdersMaxEmployee.sql
@@ -5,15 +5,15 @@ QUEST
 - 조건 : 가장 많이 주문 받은 회사 직원명과 갯수
 
 
-SELECT EMP.LASTNAME, EMP.FIRSTNAME, ORDET.Quantity
+SELECT EMP.LASTNAME, EMP.FIRSTNAME, MAX(ORDET.Quantity)
 FROM ((ORDERS AS ORD
 INNER JOIN EMPLOYEES AS EMP
 ON ORD.EmployeeID = EMP.EmployeeID)
 INNER JOIN SHIPPERS AS SHIP
 ON ORD.ShipperID = SHIP.ShipperID)
 INNER JOIN OrderDetails AS ORDET
-ON  ORD.OrderID = ORDET.OrderID
-ORDER BY ORDET.Quantity DESC LIMIT 1;
+ON  ORD.OrderID = ORDET.OrderID;
+-- ORDER BY ORDET.Quantity DESC LIMIT 1;
 
 Number of Records: 1
 LastName	FirstName	Quantity
