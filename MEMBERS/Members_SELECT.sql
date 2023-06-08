cbf09bac61d1de7ccb6e3cad78c0851e83daf728
BongWonHee <leeban1004@gmail.com>
2023-06-08 09:37:13 +0900

    add

--- MEMBERS/Members_SELECT.sql
+++ MEMBERS/Members_SELECT.sql
@@ -3,7 +3,19 @@
 -- WHERE condition;
 -- ORDER BY column1, column2, ... ASC|DESC;
 
+select NAME, AGE
+from members
+where AGE IN (32, 28)
+order by ADDRESS_CODE_ID desc;
+
+select *
+from members
+where AGE between 25 and 35
+ORDER BY GENDER_CODE_ID, AGE DESC;
+
+
 select *
 from members 
-where TELEPHONE like '%'|| '0-'||'%';
+where GENDER_CODE_ID NOT like 'G-01';
+-- WHERE GENDER_CODE_ID = 'G-01';
 
--- MEMBERS/Memers_MIN&MAX.sql
+++ MEMBERS/Memers_MIN&MAX.sql
@@ -2,5 +2,20 @@
 -- FROM table_name
 -- WHERE condition;
 
+-- 성별 당 COUNT는 각각 몇명인가?
+
+
+
+
+SELECT COUNT(*), GENDER_CODE_ID -- group by 한 것에 대한 종류를 볼때 사용한다.
+FROM MEMBERS 
+GROUP BY GENDER_CODE_ID ;
+
+SELECT COUNT(MEMBERS_ID)
+FROM MEMBERS 
+WHERE gender_code_ID = 'G-04'
+GROUP BY MEMBERS_ID;
+
+
 SELECT *
 FROM members;
